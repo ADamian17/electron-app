0bcb875b483e399b8d9d6106e4429a4f68aed856
ADamian17 <adonisdesuvikdo@gmail.com>
2021-05-14 09:14:29 -0700

    started refactor

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+// import reducer later
+
+export default combineReducers({});
---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+let middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares = [logger, ...middlewares];
+} else {
+  middlewares = [...middlewares];
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+export default store;
