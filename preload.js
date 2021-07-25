425bcb73b9e8fc1156616b65b13253d4d08db385
ADamian17 <adonisdesuvikdo@gmail.com>
2021-07-25 14:37:32 -0700

    added electron to dependencies

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@ const { ipcRenderer, contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld('electron_methods', {
   notificationApi: {
-    sendNotification: function (message) {
+    sendNotification(message) {
       ipcRenderer.send('notify', message);
     },
   },
