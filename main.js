f4df36dcdc8d53973038064f872dd53bb12f7b1a
ADamian17 <adonisdesuvikdo@gmail.com>
2021-07-29 22:55:18 -0700

    added something

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const {
-  app, BrowserWindow, Notification, ipcMain, Menu, Tray,
+  app, BrowserWindow, Notification, ipcMain, Menu, Tray, autoUpdater,
 // eslint-disable-next-line import/no-extraneous-dependencies
 } = require('electron');
+require('update-electron-app')();
+
 const path = require('path');
 const isDev = !app.isPackaged;
 
@@ -96,6 +98,34 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+const server = 'https://github.com/ADamian17/electron-app/releases/tag/v1-7-29-2021';
+const url = `${server}/update/${process.platform}/${app.getVersion()}`;
+
+autoUpdater.setFeedURL({ url });
+
+setInterval(() => {
+  autoUpdater.checkForUpdates();
+}, 60000);
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.',
+  };
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  });
+});
+
+autoUpdater.on('error', (message) => {
+  console.error('There was a problem updating the application');
+  console.error(message);
 });
 
 // chromiun web engine for rendering ui
